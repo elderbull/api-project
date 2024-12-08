cab93943b288a0c1dc2e9d70010cd3b94e4b0bae
Leon <lnathan.bull@gmail.com>
2024-12-07 21:39:56 -0500

    phase):express setup completed

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+//Import packages
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+//Check to see if environment is in production
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//Initialize Express app
+const app = express();
+
+app.use(morgan('dev')); // middleware for logging info about requests and responses
+app.use(cookieParser()); // Middleware for parsing cookies
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+  
